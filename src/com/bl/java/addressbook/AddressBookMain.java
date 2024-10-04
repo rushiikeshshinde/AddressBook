fd847d33bb87851fafdf8321e6617d13d0b78466
rushiikeshshinde <rushiikeshshiinde@gmail.com>
2024-10-04 11:42:44 +0530

    Added: UC1: Added ability to create a contact in Address Book

--- src/com/bl/java/addressbook/AddressBookMain.java
+++ src/com/bl/java/addressbook/AddressBookMain.java
@@ -1,10 +1,45 @@
 package com.bl.java.addressbook;
 
+import java.util.Scanner;
+
 public class AddressBookMain {
 
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
 
         System.out.println("Welcome to Address Book Program!");
+
+        // Taking input for contact details
+
+        System.out.println("Enter First Name;  ");
+        String firstName = in.nextLine();
+
+        System.out.println("Enter Last Name;  ");
+        String lastName = in.nextLine();
+
+        System.out.println("Enter Address");
+        String address = in.nextLine();
+
+        System.out.println("Enter City");
+        String city = in.nextLine();
+
+        System.out.println("Enter State");
+        String state = in.nextLine();
+
+        System.out.println("Enter Zip Code");
+        String zipCode = in.nextLine();
+
+        System.out.println("Enter Phone Number");
+        String phoneNumber = in.nextLine();
+
+        System.out.println("Enter Email");
+        String email = in.nextLine();
+
+        Contact contact = new Contact(firstName,lastName,address,city,state,zipCode,phoneNumber,email);
+
+
+        // Displaying the contact details
+        System.out.println("Contact Added: "+contact);
 
     }
 
